fa96ef252ec3cbc64e7e7337228d20e7aa10f407
Zach Wright <zwright@umich.edu>
2020-07-27 14:50:14 -0400

    Update CurrentFilters.js
    Remove console.log

--- src/packages/@ncigdc/components/CurrentFilters.js
+++ src/packages/@ncigdc/components/CurrentFilters.js
@@ -213,7 +213,6 @@ const CurrentFilters = (
 
           {currentFilters.map((filter, i) => {
             const value = [].concat(filter.content.value || []);
-            console.log(filter);
             return (
               <Row
                 key={`${filter.content.field}.${filter.op}.${value.join()}`}
