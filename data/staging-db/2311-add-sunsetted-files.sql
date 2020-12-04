172cd81eb496eaeb48a35928cd7b56bc6d2f1a3f
Becky Reamy <rlreamy@umich.edu>
2020-12-04 16:10:30 -0500

    KPMP-2311: Modify tables to put new primary keys in place

---
+++ data/staging-db/2311-add-sunsetted-files.sql
@@ -0,0 +1,71 @@
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('0eb39318-e618-4287-a4d5-5bcfc24fabe6', '0eb39318-e618-4287-a4d5-5bcfc24fabe6_expression_matrix.zip', '0eb39318-e618-4287-a4d5-5bcfc24fabe6', 'open', '18217685', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('0f7605fb-4b25-4225-938e-006768f0bdd8', '0f7605fb-4b25-4225-938e-006768f0bdd8_expression_matrix.zip', '0f7605fb-4b25-4225-938e-006768f0bdd8', 'open', '16463260', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('107d82df-a744-46c6-8630-2d523d348a97', '107d82df-a744-46c6-8630-2d523d348a97_expression_matrix.zip', '107d82df-a744-46c6-8630-2d523d348a97', 'open', '23692367', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('23c6cc16-64bc-4c33-bedb-04c4b702fe4b', '23c6cc16-64bc-4c33-bedb-04c4b702fe4b_expression_matrix.zip', '23c6cc16-64bc-4c33-bedb-04c4b702fe4b', 'open', '11470766', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('298dd0cb-8143-4f37-9367-271ef806bb70', '298dd0cb-8143-4f37-9367-271ef806bb70_expression_matrix.zip', '298dd0cb-8143-4f37-9367-271ef806bb70', 'open', '18214200', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('2a49402f-4a1f-4fd5-b58d-97df663fed83', '2a49402f-4a1f-4fd5-b58d-97df663fed83_expression_matrix.zip', '2a49402f-4a1f-4fd5-b58d-97df663fed83', 'open', '7879234', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('38f5fc1c-bdc5-4505-ba4c-74ff27d70998', '38f5fc1c-bdc5-4505-ba4c-74ff27d70998_expression_matrix.zip', '38f5fc1c-bdc5-4505-ba4c-74ff27d70998', 'open', '30163696', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('3e284c30-51a6-480f-a248-0497b9863486', '3e284c30-51a6-480f-a248-0497b9863486_expression_matrix.zip', '3e284c30-51a6-480f-a248-0497b9863486', 'open', '31268047', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('4337e835-4ead-4fbb-b6b0-9e06352129a6', '4337e835-4ead-4fbb-b6b0-9e06352129a6_expression_matrix.zip', '4337e835-4ead-4fbb-b6b0-9e06352129a6', 'open', '20157514', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('4db94666-2f9f-49a7-b316-2416a86b2acf', '4db94666-2f9f-49a7-b316-2416a86b2acf_expression_matrix.zip', '4db94666-2f9f-49a7-b316-2416a86b2acf', 'open', '5501292', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('70836236-3096-42f4-ade5-887d33a87efb', '70836236-3096-42f4-ade5-887d33a87efb_expression_matrix.zip', '70836236-3096-42f4-ade5-887d33a87efb', 'open', '7681785', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('777f1b36-801e-4503-8f76-ceae1e8d89a9', '777f1b36-801e-4503-8f76-ceae1e8d89a9_expression_matrix.zip', '777f1b36-801e-4503-8f76-ceae1e8d89a9', 'open', '24784972', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('77cbe198-dbd4-4cac-b63f-cf114628b7d3', '77cbe198-dbd4-4cac-b63f-cf114628b7d3_expression_matrix.zip', '77cbe198-dbd4-4cac-b63f-cf114628b7d3', 'open', '11484750', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('7963bc52-0087-41b5-a6ee-c242e315928a', '7963bc52-0087-41b5-a6ee-c242e315928a_expression_matrix.zip', '7963bc52-0087-41b5-a6ee-c242e315928a', 'open', '1346777', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('7b7c7dd1-3fef-4f6f-bf4e-4248b62cefa2', '7b7c7dd1-3fef-4f6f-bf4e-4248b62cefa2_expression_matrix.zip', '7b7c7dd1-3fef-4f6f-bf4e-4248b62cefa2', 'open', '12536547', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('7e1416c0-ab5b-4fa8-8e3b-f08fa8953cb2', '7e1416c0-ab5b-4fa8-8e3b-f08fa8953cb2_expression_matrix.zip', '7e1416c0-ab5b-4fa8-8e3b-f08fa8953cb2', 'open', '31157622', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('7eb63a19-509b-4b2b-b1a8-5b90092f5267', '7eb63a19-509b-4b2b-b1a8-5b90092f5267_expression_matrix.zip', '7eb63a19-509b-4b2b-b1a8-5b90092f5267', 'open', '13195767', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('91792ab9-651b-4393-87fb-9eb98bd51b5c', '91792ab9-651b-4393-87fb-9eb98bd51b5c_expression_matrix.zip', '91792ab9-651b-4393-87fb-9eb98bd51b5c', 'open', '11136952', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('c343aa09-ce55-48e2-b6d1-94e96c56e8ee', 'c343aa09-ce55-48e2-b6d1-94e96c56e8ee_expression_matrix.zip', 'c343aa09-ce55-48e2-b6d1-94e96c56e8ee', 'open', '25486103', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('dce98e3e-e4b9-4106-8430-7657d465b70e', 'dce98e3e-e4b9-4106-8430-7657d465b70e_expression_matrix.zip', 'dce98e3e-e4b9-4106-8430-7657d465b70e', 'open', '14057745', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('de041526-81de-448a-8042-cbc41c81a61a', 'de041526-81de-448a-8042-cbc41c81a61a_expression_matrix.zip', 'de041526-81de-448a-8042-cbc41c81a61a', 'open', '6037184', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('e4979fb1-34ea-4caf-bf34-d3320bbe24b3', 'e4979fb1-34ea-4caf-bf34-d3320bbe24b3_expression_matrix.zip', 'e4979fb1-34ea-4caf-bf34-d3320bbe24b3', 'open', '23245032', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('f3f90576-872e-473a-964b-c0854fc0c35f', 'f3f90576-872e-473a-964b-c0854fc0c35f_expression_matrix.zip', 'f3f90576-872e-473a-964b-c0854fc0c35f', 'open', '1258265', 'KPMP Pilot 1 Protocol', '7', '2', '3');
+
+INSERT INTO `file` (`dl_file_id`, `file_name`, `package_id`, `access`, `file_size`, `protocol`, `metadata_type_id`, `release_ver`, `release_sunset`)
+VALUES ('f69d8874-d97f-4c46-9115-8073d2c7aee1', 'f69d8874-d97f-4c46-9115-8073d2c7aee1_expression_matrix.zip', 'f69d8874-d97f-4c46-9115-8073d2c7aee1', 'open', '35147192', 'KPMP Pilot 1 Protocol', '7', '2', '3');
