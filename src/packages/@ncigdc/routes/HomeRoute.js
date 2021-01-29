c6177f18b8ae37fd9669a933a84fbdfb2b540db4
Becky Reamy <rlreamy@umich.edu>
2021-01-29 10:15:18 -0500

    KPMP-2379: Fix links

--- src/packages/@ncigdc/routes/HomeRoute.js
+++ src/packages/@ncigdc/routes/HomeRoute.js
@@ -5,7 +5,6 @@ import { Row, Column } from '@ncigdc/uikit/Flex';
 import Card from '@ncigdc/uikit/Card';
 import styled from '@ncigdc/theme/styled';
 import RepositoryLink from '@ncigdc/components/Links/RepositoryLink';
-import ExploreLink from '@ncigdc/components/Links/ExploreLink';
 import AnalysisLink from '@ncigdc/components/Links/AnalysisLink';
 
 
@@ -61,16 +60,16 @@ const linkStyle = {
   height: '4rem',
   borderRadius: '6px',
   transition: '0.25s ease all',
-  backgroundColor: props => props.backgroundColor || props.theme.primary,
+  backgroundColor: '#0275d8',
   ':hover': {
-    backgroundColor: props => Color(props.backgroundColor || props.theme.primary)
+    backgroundColor: props => Color('#0275d8')
       .lighten(0.2)
       .rgbString(),
   },
+  verticalAlign: 'middle',
 };
 
 const Repository = styled(RepositoryLink, linkStyle);
-const Explore = styled(ExploreLink, linkStyle);
 const Analysis = styled(AnalysisLink, linkStyle);
 
 const Home = () => (
@@ -88,7 +87,7 @@ const Home = () => (
           <Header>Atlas Analyzer</Header>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
           <Row>
-            <Analysis backgroundColor="#0275d8"><span style={{ verticalAlign: 'middle' }}>Go to Analysis</span></Analysis>
+            <Analysis backgroundColor="#0275d8">Go to Analysis</Analysis>
           </Row>
         </NavigationCard>
       </HomeRouteRow>
@@ -97,7 +96,15 @@ const Home = () => (
           <Header>Atlas Explorer</Header>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
           <Row>
-            <Explore backgroundColor="#0275d8"><span style={{ verticalAlign: 'middle' }}>Go to Explorer</span></Explore>
+            <Column style={{
+              margin: '1rem',
+              width: '25%',
+              flexDirection: 'column',
+              flexWrap: 'wrap',
+              color: ({theme}) => theme.greyScale7 || 'silver',
+            }}>
+              <span style={linkStyle}><a href="/explorer" alt="Explorer tool">Go to Explorer</a></span>
+            </Column>
           </Row>
         </NavigationCard>
       </HomeRouteRow>
@@ -106,7 +113,7 @@ const Home = () => (
           <Header>Atlas Repository</Header>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
           <Row>
-            <Repository backgroundColor="#0275d8"><span style={{ verticalAlign: 'middle' }}>Go to Repository</span></Repository>
+            <Repository backgroundColor="#0275d8">Go to Repository</Repository>
           </Row>
         </NavigationCard>
       </HomeRouteRow>
