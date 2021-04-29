75651be73e97d8d614ed08c78adcece2acfab01d
Zach Wright <zwright@umich.edu>
2021-04-29 14:47:19 -0400

    KPMP-2525: Not found page update

--- src/packages/@ncigdc/components/NotFound.js
+++ src/packages/@ncigdc/components/NotFound.js
@@ -1,11 +1,28 @@
 // @flow
 import React from 'react';
 import HomeLink from '@ncigdc/components/Links/HomeLink';
+import {Button, Col, Row} from "reactstrap";
 
 export default () => (
-  <div className="container-fluid text-center not-found">
-    <h1>Page Not Found</h1>
-    <p>{`You have searched for something that doesn't exist!`}</p>
-    <HomeLink>Go back to the home page.</HomeLink>
-  </div>
+    <article className="container" id='not-found-page'>
+        <Row id="not-found-container" className='mr-5 p-5'>
+            <Col xs={12} md={4}>
+                <img className='not-found-image' src="img/404-img.svg" alt="Page not found" id="oops-image"/>
+            </Col>
+            <Col xs={12} md={8} className='not-found-text'>
+                <p className="not-found-regular">Sorry. We couldn't find the page you're looking for.</p>
+                <p className="not-found-small">
+                    If you're still having problems, please contact <a href='mailto:admin@kpmp.org'>admin@kpmp.org</a>
+                </p>
+                <p className="oops-button-container">
+                    <Button color='primary'
+                            className="btn btn-primary"
+                            onClick={() => (window.location.href = '/repository')}
+                    >
+                        Back to Home
+                    </Button>
+                </p>
+            </Col>
+        </Row>
+    </article>
 );
